30344e640e7c699541f455541921582c830ddf16
GritAndrey <stendarr@gmail.com>
2021-06-10 09:43:58 +0100

    saveResult method

--- src/main/java/ProxyChecker/Main.java
+++ src/main/java/ProxyChecker/Main.java
@@ -1,31 +1,33 @@
 package ProxyChecker;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
     static final String PATH = "ip";
+    static final String RESULTPATH = "good_ip";
+
     public static void main(String[] args) {
         try {
             FileInputStream fis = new FileInputStream(PATH);
             int i;
             String proxy = "";
-            while((i=fis.read()) != -1){
-                if(i==13) continue;
-                else if(i==10){
+            while ((i = fis.read()) != -1) {
+                if (i == 13) continue;
+                else if (i == 10) {
                     String ip = proxy.split(":")[0];
                     String port = proxy.split(":")[1];
                     checkProxy(ip, Integer.parseInt(port));
                     proxy = "";
-                }else if(i!=9){
+                } else if (i != 9) {
                     proxy += (char) i;
-                }else{
+                } else {
                     proxy += ":";
                 }
             }
@@ -35,21 +37,31 @@ public class Main {
 
     }
 
-    public static void checkProxy(String ip, int port){
+    public static void checkProxy(String ip, int port) {
         try {
-            Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip,port));
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
             URL url = new URL("https://vozhzhaev.ru/test.php");
             URLConnection urlConnection = url.openConnection(proxy);
             InputStream is = urlConnection.getInputStream();
             InputStreamReader reader = new InputStreamReader(is);
             int i;
             StringBuilder result = new StringBuilder();
-            while ((i=reader.read()) != -1){
-                result.append((char)i);
+            while ((i = reader.read()) != -1) {
+                result.append((char) i);
             }
             System.out.println(result);
+            saveResult(result.toString() + ":" + port);
         } catch (IOException exception) {
-            System.out.println(ip+" - down!");
+            System.out.println(ip + " - down!");
+        }
+    }
+
+    private static void saveResult(String ipPort) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(RESULTPATH), StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+            writer.write(ipPort);
+            writer.write("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
