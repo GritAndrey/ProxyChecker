9bd192cf3619486ba4f0eba01f6d57e3cd5f5499
GritAndrey <stendarr@gmail.com>
2021-06-10 09:16:57 +0100

    added ip to gitignore and copied main method from lecture

--- src/main/java/ProxyChecker/Main.java
+++ src/main/java/ProxyChecker/Main.java
@@ -1,7 +1,55 @@
 package ProxyChecker;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Main {
+    static final String PATH = "ip";
     public static void main(String[] args) {
-        System.out.println("It Works!");
+        try {
+            FileInputStream fis = new FileInputStream(PATH);
+            int i;
+            String proxy = "";
+            while((i=fis.read()) != -1){
+                if(i==13) continue;
+                else if(i==10){
+                    String ip = proxy.split(":")[0];
+                    String port = proxy.split(":")[1];
+                    checkProxy(ip, Integer.parseInt(port));
+                    proxy = "";
+                }else if(i!=9){
+                    proxy += (char) i;
+                }else{
+                    proxy += ":";
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void checkProxy(String ip, int port){
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip,port));
+            URL url = new URL("https://vozhzhaev.ru/test.php");
+            URLConnection urlConnection = url.openConnection(proxy);
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(is);
+            int i;
+            StringBuilder result = new StringBuilder();
+            while ((i=reader.read()) != -1){
+                result.append((char)i);
+            }
+            System.out.println(result);
+        } catch (IOException exception) {
+            System.out.println(ip+" - down!");
+        }
     }
 }
